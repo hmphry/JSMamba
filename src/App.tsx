2ff41a0c6358d65b662e22b417769fd5ee62ea48
Emily Humphrey <emily@hmphry.com>
2023-01-29 01:50:03 -0500

    wip: clean up structure and begin logic building

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,46 @@
-import { useState, useRef } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import Button from './components/Button'
+import { useState, useEffect } from 'react'
+import QuestionPlate from './components/QuestionPlate'
 
 function App() {
-  const cardRef = useRef(null);
-  const [flipped, setFlipped] = useState(false);
+  const [question, setQuestion] = useState({});
+  const [questionCount, setQuestionCount] = useState(0);
 
-  function handleClick() {
-    // console.log(cardRef.current.classList.add(""))
-    setFlipped( !flipped );
+  useEffect(() => {
+    getQuestion()
+  }, [])
+  
+
+  function getQuote() {}
+  function getQuestionSet() {
+    setQuestionCount( 1 )
   }
+  function getQuestion() {
+    setQuestion({
+      text: "The Array.pop() method removes the last element from an array and returns that element. This method changes the length of the array.",
+      correct_answer: true
+    })
+  }
+  function answerQuestion(response:unknown) {
+    if(response === question.correct_answer) {
+      console.log("Yay!");
+    } else if(response === "skip") {
+      console.log("No worries!");
+    } else {
+      console.log("Better luck next time!")
+    }
+  }
+  function nextQuestion() {}
 
   return (
     <div className="App bg-yellow-200">
-      <div className='w-full max-w-7xl px-8 lg:px-0 lg:mx-auto'>
+      <div className='w-full max-w-7xl px-8 lg:mx-auto'>
         <div className='grid grid-cols-8 gap-8 h-screen items-center'>
           <div className='col-span-3'>
             <h1 className='mb-4 text-2xl text-purple-700'>JSMamba</h1>
             <p className='pb-2'>"It's not about the number of hours you practice, it's about the number of hours your mind is present during the practice."</p>
           </div>
           <div className='col-span-5'>
-            <div className={`transform-gpu transition-all duration-500 [transform-style:preserve-3d] bg-white rounded-md p-8 shadow-lg ${flipped ? '[transform:rotateY(180deg)]' : false}`}>
-              <div className='[backface-visibility:hidden]'>
-                <p className='mb-2 text-sm text-gray-400'>Question 1 of 10</p>
-                <p className='text-2xl'>The Array.pop() method removes the last element from an array and returns that element. This method changes the length of the array.</p>
-                <div className='flex justify-center gap-4 mt-8'>
-                  <Button clickHandler={handleClick} className="border-gray-400 text-gray-400 hover:border-gray-800 hover:text-gray-800">True</Button>
-                  <Button clickHandler={handleClick} className="border-gray-400 text-gray-400 hover:border-gray-800 hover:text-gray-800">False</Button>
-                  <Button clickHandler={handleClick} className="border-gray-400 text-gray-400 hover:border-gray-800 hover:text-gray-800">Unsure</Button>
-                </div>
-              </div>
-              <div className='absolute top-0 bottom-0 left-0 right-0 flex items-center justify-center [backface-visibility:hidden] [transform:rotateY(180deg)]'>
-                <div>
-                  <p className='text-2xl text-center'>That was <span>correct!</span></p>
-                  <div className='flex justify-center mt-8'>
-                    <Button clickHandler={handleClick}>Next Question</Button>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <QuestionPlate count={questionCount} question={question.text} handleAnswer={answerQuestion} nextQuestion={nextQuestion}></QuestionPlate>
           </div>
         </div>
       </div>
